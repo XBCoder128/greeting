877e6f734e30f1d96266f0eb13b37521059197fd
XBCoder <nwyfnck@163.com>
2022-06-10 19:06:08 +0800

    test

--- greeting.go
+++ greeting.go
@@ -3,7 +3,11 @@ package greeting
 import "fmt"
 
 func Hello(name string) string {
-
 	msg := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return msg
+}
+
+func Bye(name string) string {
+	msg := fmt.Sprintf("Oh, %v. Bye!", name)
 	return msg
 }
